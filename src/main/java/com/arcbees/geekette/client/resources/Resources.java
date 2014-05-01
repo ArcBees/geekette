07052c11b64cedcfc31d368aa3606ca171085725
larry <larry.matte@arcbees.com>
2014-05-01 19:36:57 -0400

    htmlCss

--- src/main/java/com/arcbees/geekette/client/resources/Resources.java
+++ src/main/java/com/arcbees/geekette/client/resources/Resources.java
@@ -16,16 +16,202 @@
 
 package com.arcbees.geekette.client.resources;
 
+import org.vectomatic.dom.svg.ui.SVGResource;
+
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.DataResource;
+import com.google.gwt.resources.client.ImageResource;
 
 public interface Resources extends ClientBundle {
     public interface Styles extends CssResource {
+        String height();
+
         String clearfix();
 
-        String inbl();
+        String section();
+
+        String bubble();
+
+        String wrapper();
+
+        String wrapperBubble();
+
+        String txtcenter();
+
+        String logo();
+
+        String price();
+
+        String title();
+
+        String date();
+
+        String place();
+
+        String ticket();
+
+        String partager();
+
+        String share();
+
+        String tt();
+
+        String tooltip();
+
+        String bubbleWhite();
+
+        String logoCrown();
+
+        String txtPink();
+
+        String flip3D();
+
+        String front();
+
+        String back();
+
+        String dict();
+
+        String bubbleArcbees();
+
+        String left();
+
+        String logoArcbees();
+
+        String moveFastest();
+
+        String moveFaster();
+
+        String moveFast();
+
+        String moveMed();
+
+        String moveMed2();
+
+        String moveMed3();
+
+        String moveSlow();
+
+        String moveSlower();
+
+        String moveSlowest();
+
+        String bubble1();
+
+        String bubble2();
+
+        String bubble3();
+
+        String bubble4();
+
+        String bubble5();
+
+        String bubble6();
+
+        String bubble7();
+
+        String bubble8();
+
+        String bubble9();
+
+        String backTop();
     }
 
     @Source("styles.css")
     Styles styles();
+
+    ImageResource logo();
+
+    ImageResource logoCrown();
+
+    ImageResource backTop();
+
+    SVGResource bubble1();
+
+    SVGResource bubble2();
+
+    SVGResource bubble3();
+
+    SVGResource bubble4();
+
+    SVGResource bubble5();
+
+    SVGResource bubble6();
+
+    SVGResource bubble7();
+
+    SVGResource bubble8();
+
+    SVGResource bubble9();
+
+    SVGResource logoArcbees();
+
+    SVGResource iconMail();
+
+    SVGResource iconFb();
+
+    SVGResource iconLkin();
+
+    SVGResource iconTwit();
+
+    @Source("baron/baron_neue-webfont.eot")
+    DataResource baronNeueEot();
+
+    @Source("baron/baron_neue-webfont.woff")
+    DataResource baronNeueWoff();
+
+    @Source("baron/baron_neue-webfont.svg")
+    DataResource baronNeueSvg();
+
+    @Source("baron/baron_neue-webfont.ttf")
+    DataResource baronNeueTtf();
+
+    @Source("baron/baron_neue_black-webfont.eot")
+    DataResource baronNeueBlackEot();
+
+    @Source("baron/baron_neue_black-webfont.ttf")
+    DataResource baronNeueBlackTtf();
+
+    @Source("baron/baron_neue_black-webfont.svg")
+    DataResource baronNeueBlackSvg();
+
+    @Source("baron/baron_neue_black-webfont.woff")
+    DataResource baronNeueBlackWoff();
+
+    @Source("brandongrotesque/brandon_bld-webfont.eot")
+    DataResource brandonGrotesqueBoldEot();
+
+    @Source("brandongrotesque/brandon_bld-webfont.svg")
+    DataResource brandonGrotesqueBoldSvg();
+
+    @Source("brandongrotesque/brandon_bld-webfont.ttf")
+    DataResource brandonGrotesqueBoldTtf();
+
+    @Source("brandongrotesque/brandon_bld-webfont.woff")
+    DataResource brandonGrotesqueBoldWoff();
+
+    @Source("brandongrotesque/brandon_light-webfont.eot")
+    DataResource brandonGrotesqueLightEot();
+
+    @Source("brandongrotesque/brandon_light-webfont.svg")
+    DataResource brandonGrotesqueLightSvg();
+
+    @Source("brandongrotesque/brandon_light-webfont.ttf")
+    DataResource brandonGrotesqueLightTtf();
+
+    @Source("brandongrotesque/brandon_light-webfont.woff")
+    DataResource brandonGrotesqueLightWoff();
+
+    @Source("brandongrotesque/brandon_reg-webfont.eot")
+    DataResource brandonGrotesqueRegEot();
+
+    @Source("brandongrotesque/brandon_reg-webfont.svg")
+    DataResource brandonGrotesqueRegSvg();
+
+    @Source("brandongrotesque/brandon_reg-webfont.ttf")
+    DataResource brandonGrotesqueRegTtf();
+
+    @Source("brandongrotesque/brandon_reg-webfont.woff")
+    DataResource brandonGrotesqueRegWoff();
 }
