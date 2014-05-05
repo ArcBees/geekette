99b8a99952be5a7423126df4ec5bce110fe5319b
christiangoudreau <christian.goudreau@arcbees.com>
2014-05-05 16:52:57 -0400

    Added share links on share elements

--- src/main/java/com/arcbees/geekette/client/application/ApplicationView.java
+++ src/main/java/com/arcbees/geekette/client/application/ApplicationView.java
@@ -20,11 +20,16 @@ import javax.inject.Inject;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
 
 public class ApplicationView extends ViewWithUiHandlers<ApplicationUiHandlers> implements ApplicationPresenter.MyView {
+    @UiField
+    HTMLPanel main;
+
     interface Binder extends UiBinder<Widget, ApplicationView> {
     }
 
